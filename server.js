42821492510b9bb56d08a3e80a5cf8f95ff3b1d7
Bonifase <orwabonifase@gmail.com>
2019-04-14 17:26:25 +0300

    ft(connect to database) connect to database
      -connect to mongo database
      -set up routes

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
+
+//connect to mongoDB
+const db = mongoose.connect('mongodb://localhost/developerAPI', { useNewUrlParser: true }).then(() => console.log("Database is connected succesfully!!")).catch(() => console.log("Database connection failed."));
 
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('<h1>Hello developer</h1>'));
+
+//use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
